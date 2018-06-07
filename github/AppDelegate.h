2be9aaafefa438566857917d0a09148af82c8d12
kwldx <kwldx1@hotmail.com>
2018-06-07 14:54:48 +0800

    Initial Commit

---
+++ github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by kwldx on 2018/6/7.
+//  Copyright © 2018年 kwldx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
